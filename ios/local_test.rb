be5f0c20456d746e4001b38f64cba4165baabb98
Saurav Kothari <sauravkothari2@gmail.com>
2017-10-06 19:50:49 +0530

    iOS Local tests

--- ios/local_test.rb
+++ ios/local_test.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'appium_lib'
+require 'browserstack/local'
 require 'pry'
 
 device =   'iPhone 7'
@@ -9,15 +10,23 @@ access_key = ENV['BROWSERSTACK_ACCESSKEY']
 caps = {}
 caps['device'] = 'iPhone 7 Plus'
 caps['realMobile'] = true
+caps['browserstack.local'] = true
 caps['browserstack.debug'] = true
-caps['app'] = 'bs://206df813c42ead2181fd2526c2af674116fefacb'
+caps['app'] = 'bs://f06c0cf5c524b636f174f32d4572519656fe4d73'
 
 caps['project'] = 'Sinstein'
 caps['build'] = 'Ruby Appium Sample'
-caps['name'] = 'iOS Single Test'
+caps['name'] = 'iOS Local Test'
+
 # Android App bs://953cfe54a252dcf446117f94b51aaf20fb644441
 # Android local bs://025cc446c42c0c84036ab0d23aded1a64ce97b71
 # IOS App bs://206df813c42ead2181fd2526c2af674116fefacb
+# IOS Local bs://f06c0cf5c524b636f174f32d4572519656fe4d73
+
+
+bs_local = BrowserStack::Local.new
+bs_local_args = { "key" => access_key }
+bs_local.start(bs_local_args)
 
 appium_driver = Appium::Driver.new({'caps' => caps, 'appium_lib' => { :server_url => "http://#{username}:#{access_key}@hub.browserstack.com/wd/hub"}})
 driver = appium_driver.start_driver
@@ -25,21 +34,14 @@ driver = appium_driver.start_driver
 
 puts driver.session_id
 
-element = driver.find_element(:accessibility_id, "Log In")
+element = driver.find_element(:accessibility_id, "TestBrowserStackLocal")
 element.click
 
 sleep 10
 
-element = driver.find_element(:accessibility_id, "Email address")
-element.send_keys("hello@browserstack.com")
+element = driver.find_element(:accessibility_id, "ResultBrowserStackLocal")
 
-driver.find_element(:accessibility_id, "Next").click
-
-sleep 10
-
-results = driver.find_elements(:xpath, "//XCUIElementTypeStaticText")
-
-if results.map(&:text).any?{|x| x.match('not registered')}
+if element.text.match("Up and running")
   puts "FOUND RESULTS"
 else
   puts "FOUND NOTHING"
@@ -47,3 +49,5 @@ end
 
 driver.quit
 appium_driver.driver_quit
+
+bs_local.stop
